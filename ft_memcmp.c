0b87f309dd5c77e2d48d2b4e8e6f1af33f231878
jciglesias <jiglesia@student.42.fr>
2019-10-12 18:06:36 +0200

    to finish isalpha

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,5 +14,16 @@
 
 int		ft_memcmp(const coid *s1, const void *s2, size_t n)
 {
+  unsigned char *t1;
+  unsigned char *t2;
 
+  t1 = (unsigned char *)s1;
+  t2 = (untigned char *)s2;
+
+  while (n-- > 0)
+    {
+      if (t1++ == t2++)
+	return (--t1 - --t2);
+    }
+  return (t1 - t2);
 }
