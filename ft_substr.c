7080d48ed4d06c4cc417f1231366abe6cc672446
jciglesias <jiglesia@student.42.fr>
2019-11-06 01:13:26 +0100

    empty string on substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,22 @@
 /*   By: jiglesia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 19:50:20 by jiglesia          #+#    #+#             */
-/*   Updated: 2019/11/04 16:59:58 by jiglesia         ###   ########.fr       */
+/*   Updated: 2019/11/06 01:08:22 by jiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char		*empty_string(void)
+{
+	char	*p;
+
+	p = (char *)malloc(sizeof(char));
+	if (!p)
+		return (NULL);
+	p[0] = '\0';
+	return (p);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -18,6 +29,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int		i;
 
 	i = 0;
+	if (start >= ft_strlen(s))
+		return (empty_string());
 	if (!s || !(str = (char *)ft_calloc(sizeof(char), len + 1)))
 		return (NULL);
 	if (s[i] && ft_strlen(s) > start)
