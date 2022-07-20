c238dee66e69d347fbac8942862cd147f34b03cc
jciglesias <jiglesia@student.42.fr>
2022-07-20 16:42:27 +0200

    simplified

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,23 +6,16 @@
 /*   By: jiglesia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 19:40:27 by jiglesia          #+#    #+#             */
-/*   Updated: 2019/10/30 21:32:42 by jiglesia         ###   ########.fr       */
+/*   Updated: 2022/07/20 16:41:01 by jiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void		ft_putint(int nb, int fd)
-{
-	if (nb > 9)
-		ft_putint(nb / 10, fd);
-	ft_putchar_fd(nb % 10 + '0', fd);
-}
-
-static void		ft_putnint(unsigned int n, int fd)
+static void		ft_putint(unsigned int n, int fd)
 {
 	if (n > 9)
-		ft_putnint(n / 10, fd);
+		ft_putint(n / 10, fd);
 	ft_putchar_fd(n % 10 + '0', fd);
 }
 
@@ -30,12 +23,11 @@ void			ft_putnbr_fd(int n, int fd)
 {
 	unsigned int nb;
 
+	nb = (unsigned int)n;
 	if (n < 0)
 	{
 		nb = -n;
 		ft_putchar_fd('-', fd);
-		ft_putnint(nb, fd);
 	}
-	else
-		ft_putint(n, fd);
+	ft_putint(nb, fd);
 }
